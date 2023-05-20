b27977bc2d41f1982c9fc5cd03343f07947f503c
Bahram Movlanov <bmovlanov@gmail.com>
2023-05-20 13:17:37 -0600

    cleanup and add `atom/Color`

---
+++ packages/react/src/atoms/Color/Color.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface ColorProps {
+  hexCode: string;
+}
+
+const Color: React.FC<ColorProps> = ({ hexCode }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: hexCode,
+      }}
+    ></div>
+  );
+};
+
+export default Color;
