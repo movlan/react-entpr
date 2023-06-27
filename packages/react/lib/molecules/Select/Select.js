ec2183f31c385798a2bf400a81726df919b671fe
Bahram Movlanov <bahram.movlanov@passportinc.com>
2023-06-27 12:17:15 -0600

    lecture 36

--- packages/react/lib/molecules/Select/Select.js
+++ packages/react/lib/molecules/Select/Select.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react';
+import Text from '../../atoms/Text/Text.js';
 
 const Select = (props) => {
     const { onOptionSelected: handler, options = [], label = 'Please select an option...' } = props;
     const [isOpen, setIsOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(null);
     const onOptionSelected = (option, optionIndex) => {
-        setIsOpen((prevState) => !prevState);
         if (handler) {
             handler(option, optionIndex);
         }
+        setSelectedIndex(optionIndex);
+        setIsOpen(false);
     };
     const onLabelClick = () => {
         setIsOpen((prevState) => !prevState);
     };
     return (React.createElement("div", { className: "dse-select" },
         React.createElement("button", { className: "dse-select__label", onClick: onLabelClick },
-            label,
+            React.createElement(Text, null, selectedIndex === null ? label : options[selectedIndex].label),
             React.createElement("svg", { width: "1rem", height: "1rem", xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", strokeWidth: 1.5, stroke: "currentColor", className: "w-6 h-6" },
                 React.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", d: "M19.5 8.25l-7.5 7.5-7.5-7.5" }))),
-        isOpen ? (React.createElement("ul", { className: "dse-select__overlay" }, options.map((option, idx) => (React.createElement("li", { className: "dse-select__option", key: option.value, onClick: () => onOptionSelected(option, idx) }, option.label))))) : null));
+        isOpen ? (React.createElement("ul", { className: "dse-select__overlay" }, options.map((option, idx) => {
+            const isSelected = idx === selectedIndex;
+            return (React.createElement("li", { className: `dse-select__option ${isSelected ? 'dse-select__option--selected' : ''}`, key: option.value, onClick: () => onOptionSelected(option, idx) },
+                React.createElement(Text, null, option.label),
+                isSelected && (React.createElement("svg", { width: "1rem", height: "1rem", xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", strokeWidth: 1.5, stroke: "currentColor", className: "w-6 h-6" },
+                    React.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", d: "M4.5 12.75l6 6 9-13.5" })))));
+        }))) : null));
 };
 
 export { Select as default };
--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Text from '../../atoms/Text/Text';
 
 interface SelectOption {
   label: string;
@@ -15,12 +16,14 @@ const Select = (props: SelectProps) => {
   const { onOptionSelected: handler, options = [], label = 'Please select an option...' } = props;
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedIndex, setSelectedIndex] = useState<null | number>(null);
 
   const onOptionSelected = (option: SelectOption, optionIndex: number) => {
-    setIsOpen((prevState) => !prevState);
     if (handler) {
       handler(option, optionIndex);
     }
+    setSelectedIndex(optionIndex);
+    setIsOpen(false);
   };
 
   const onLabelClick = () => {
@@ -30,7 +33,7 @@ const Select = (props: SelectProps) => {
   return (
     <div className="dse-select">
       <button className="dse-select__label" onClick={onLabelClick}>
-        {label}
+        <Text>{selectedIndex === null ? label : options[selectedIndex].label}</Text>
         <svg
           width="1rem"
           height="1rem"
@@ -46,15 +49,32 @@ const Select = (props: SelectProps) => {
       </button>
       {isOpen ? (
         <ul className="dse-select__overlay">
-          {options.map((option, idx) => (
-            <li
-              className="dse-select__option"
-              key={option.value}
-              onClick={() => onOptionSelected(option, idx)}
-            >
-              {option.label}
-            </li>
-          ))}
+          {options.map((option, idx) => {
+            const isSelected = idx === selectedIndex;
+            return (
+              <li
+                className={`dse-select__option ${isSelected ? 'dse-select__option--selected' : ''}`}
+                key={option.value}
+                onClick={() => onOptionSelected(option, idx)}
+              >
+                <Text>{option.label}</Text>
+                {isSelected && (
+                  <svg
+                    width="1rem"
+                    height="1rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                  </svg>
+                )}
+              </li>
+            );
+          })}
         </ul>
       ) : null}
     </div>
