c3ee615e5e17181b82866626ea7ab20bf8b8621d
Bahram Movlanov <bmovlanov@gmail.com>
2023-06-27 21:03:09 -0600

    setup storybook

--- packages/react/src/molecules/Select/Select.stories.tsx
+++ packages/react/src/molecules/Select/Select.stories.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Select from './Select';
 
+import '@movlan/scss/lib/Select.css';
+
+const options = [
+  {
+    label: 'Strict Black',
+    value: 'strict-black',
+  },
+  {
+    label: 'Heavenly Green',
+    value: 'heavenly-green',
+  },
+  {
+    label: 'Sweet Pink',
+    value: 'pink',
+  },
+];
+
 export default {
   title: 'Select',
 };
 
-export const Common = () => <Select options={[]} />;
+export const Common = () => <Select options={options} />;
