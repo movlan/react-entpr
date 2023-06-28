9f2e7fc1aa948b013eeca580c6f6a7b4312d01dc
Bahram Movlanov <bmovlanov@gmail.com>
2023-06-27 22:52:01 -0600

    update git

--- packages/react/lib/atoms/Color/Color.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spacing } from '@movlan/foundation';
-interface ColorProps {
-    hexCode: string;
-    width?: keyof typeof Spacing;
-    height?: keyof typeof Spacing;
-}
-declare const Color: (props: ColorProps) => React.JSX.Element;
-export default Color;
