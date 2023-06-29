8e080bc259890d6e35228f0dd1d98b65565a57e3
Bahram <57245706+movlan@users.noreply.github.com>
2023-06-29 01:21:18 -0600

    Delete ready-files directory

--- ready-files/packages/react/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    stories: ['../src/**/*.stories.tsx'],
-    addons: [
-        '@storybook/preset-typescript',
-        '@storybook/addon-storysource',
-        '@storybook/addon-knobs',
-        '@storybook/addon-a11y'
-    ]
-}
--- ready-files/packages/react/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    roots: ['<rootDir>/src'],
-    testRegex: '(/.*\\.test)\\.(ts|tsx)$',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
-}
